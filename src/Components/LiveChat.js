49cab686796a6abcda503f3b1b64bf08f55416fb
aiswarya-cs <aiswaryacs34@gmail.com>
2024-01-30 12:22:10 +0530

    inital commit

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -6,58 +6,61 @@ import { generateRandomMessages, generateRandomname } from '../Utils/helper';
 import { IoIosSend } from 'react-icons/io';
 
 const LiveChat = () => {
-  const [LiveMessage, setLiveMessage] = useState ("")
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
-  const ChatMessages = useSelector((store) => store.chat.messages);
+  const chatMessages = useSelector((store) => store.chat.messages);
 
   useEffect(() => {
     // API POLLING
-    const i = setInterval(() => {
-      dispatch(
-        addMessage({
+    const intervalId = setInterval(() => {
+      dispatch({
+        type: addMessage.type,
+        payload: {
           name: generateRandomname(),
           message: generateRandomMessages(20),
-        })
-      );
-      setLiveMessage("");
+        },
+      });
     }, 1500);
-    return () => clearInterval(i);
-  }, []);
+
+    return () => clearInterval(intervalId);
+  }, [dispatch]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: addMessage.type,
+      payload: {
+        name: "Aiswarya",
+        message: liveMessage,
+      },
+    });
+    setLiveMessage("");
+  };
 
   return (
     <>
-    <div className='w-3/4 h-[500px] mx-20 border-slate-700 rounded- rounded-xl border flex flex-col-reverse bg-gray-100 overflow-y-hidden overflow-y-scroll'>
-      <div>
-      {ChatMessages.map((c) => (
-        <ChatMessage name={c.name} message={c.message} />
-      ))}
+      <div className='w-3/4 h-[500px] mx-20 border-slate-700 rounded- rounded-xl border flex flex-col-reverse bg-gray-100 overflow-y-hidden overflow-y-scroll'>
+        <div>
+          {chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
       </div>
-      
-    </div>
-     <form 
-        className=' flex w-4/5 mx-10  rounded-b-full border-blue-100 h-16'
-        onSubmit={(e)=>{
-          e.preventDefault();
-          /*console.log(LiveMessage);*/
-          dispatch(addMessage({
-            name:"Aiswarya",
-            message:LiveMessage,
-          }))
-        }}
-        >
-     
-       <input type='text' 
-              value={LiveMessage}
-              placeholder='whats your thought..'
-              onChange={(e)=>{setLiveMessage(e.target.value);
-              }}
-              className='flex w-4/5 ml-12 border mt-4 border-gray-500  rounded-full ' 
-      />
-
-       <button className='cursor-pointer mx-1'>
-        <IoIosSend className='text-4xl'/>
+      <form
+        className='flex w-4/5 mx-10 rounded-b-full border-blue-100 h-16'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type='text'
+          value={liveMessage}
+          placeholder="What's your thought..."
+          onChange={(e) => setLiveMessage(e.target.value)}
+          className='flex w-4/5 ml-12 border mt-4 border-gray-500 rounded-full'
+        />
+        <button type='submit' className='cursor-pointer mx-1'>
+          <IoIosSend className='text-4xl' />
         </button>
-     </form> 
+      </form>
     </>
   );
 };
