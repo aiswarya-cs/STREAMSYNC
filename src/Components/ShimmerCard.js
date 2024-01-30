5a4f27e990713d4cebbe6f9ffab5b5f87f84f0d7
aiswarya-cs <aiswaryacs34@gmail.com>
2024-01-30 12:06:51 +0530

    second commit

--- src/Components/ShimmerCard.js
+++ src/Components/ShimmerCard.js
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const ShimmerCard = () => {
   return (
     <div className='flex flex-wrap'>
-        {Array(10).fill(" ").map((e)=>{
-            <div className='p-2 m-1 w-72 h-80 shadow-lg rounded-md text-left'>
-            <div className='rounded-lg'  
-          alt='thumbnails'/>
-           <ul>
-              <li className='font-bold p-2 ml-16 h-5 w-52 my-1 rounded-md bg-slate-300 '> </li>
-              <li className='font-bold py-2 bg-slate-300 my-1 rounded-md h-6'>   </li>
-              <li className='font-bold py-2 my-1 bg-slate-300 h-6 rounded-md'></li>
-           </ul>
-      
+      {Array(10).fill(" ").map((e, index) => (
+        <div key={index} className='p-2 m-1 w-72 h-80 shadow-lg rounded-md'>
+          <div className='rounded-lg w-70 h-40 bg-slate-300'>
+          </div>
+          <ul>
+            <li className='font-bold p-2 ml-16 h-5 w-52 my-1 rounded-md bg-slate-300'> </li>
+            <li className='font-bold py-2 bg-slate-300 my-1 rounded-md h-6'> </li>
+            <li className='font-bold py-2 my-1 bg-slate-300 h-6 rounded-md'></li>
+          </ul>
         </div>
-        })}
-        
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ShimmerCard
+export default ShimmerCard;
